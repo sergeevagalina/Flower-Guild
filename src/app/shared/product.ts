a3ac747bf91747c7dcd406c9b48cc121e98b54f6
Galina Sergeeva <sergeevagalina.al@gmail.com>
2017-10-30 21:08:21 +0300

    fixed paths for assets 2

--- src/app/shared/product.ts
+++ src/app/shared/product.ts
@@ -13,16 +13,16 @@ export class Product {
 export const DealType = ['Продажа', 'Обмен', 'Продажа / Обмен'];
 
 export const Images = [
-    '/assets/images/plant.svg',
-    '/assets/images/hibiscus.svg',
-    '/assets/images/cactus.svg',
-    '/assets/images/geranium.svg',
-    '/assets/images/hydrangea.svg',
-    '/assets/images/kalla.svg',
-    '/assets/images/orchid.svg',
-    '/assets/images/succulent.svg',
-    '/assets/images/spathiphyllum.svg',
-    '/assets/images/violet.svg'
+    'assets/images/plant.svg',
+    'assets/images/hibiscus.svg',
+    'assets/images/cactus.svg',
+    'assets/images/geranium.svg',
+    'assets/images/hydrangea.svg',
+    'assets/images/kalla.svg',
+    'assets/images/orchid.svg',
+    'assets/images/succulent.svg',
+    'assets/images/spathiphyllum.svg',
+    'assets/images/violet.svg'
 ];
 
 export const ContactType = ['Тел.', 'Email'];
